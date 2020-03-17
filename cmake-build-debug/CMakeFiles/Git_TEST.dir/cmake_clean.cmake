d7305fccf957a93bc94611187121d7610bce7650
Qant1 <51907758+Quentin8@users.noreply.github.com>
2020-03-17 17:48:10 +0100

    Commit 1

---
+++ cmake-build-debug/CMakeFiles/Git_TEST.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Git_TEST.dir/main.cpp.o"
+  "Git_TEST.pdb"
+  "Git_TEST"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Git_TEST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Git_TEST.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Git_TEST.
+# This may be replaced when dependencies are built.
